9bae6cb88588f8acec82ded7e0574a3e696955b5
Chinwilla <bogdandrozd123@gmail.com>
2025-09-14 18:58:15 +0100

    Updata

--- src/server.js
+++ src/server.js
@@ -1,68 +1,66 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import { getAllContacts, getContactById } from './services/contacts.js';
-
+dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
-
 export function setupServer() {
-    const app = express();
+  const app = express();
 
-    app.use(express.json());
-    app.use(cors());
-
-      app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-    );
-
-
-	app.get('/contacts', async (req, res) => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'It is working!',
+    });
+  });
+  app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
-
     res.status(200).json({
-       status: 200,
-  message: "Successfully found contacts!",
+      status: 200,
+      message: 'Successfully found contacts!',
       data: contacts,
     });
   });
-
   app.get('/contacts/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
-
-	if (!contact) {
-	  res.status(404).json({
-		 message: 'Contact not found',
-	  });
-	  return;
-	}
-
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
     res.status(200).json({
       status: 200,
-	message: `Successfully found contact with id ${contactId}!`,
-      data: {contact},
+      message: 'Successfully found contact with id {contactId}!',
+      data: contact,
     });
   });
-
-  app.use((req, res, next) => {
-    res.status(404).json({ status: 404, message: 'Not found' });
+  app.use((req, res) => {
+    console.log('all check');
+    res.status(404).json({
+      message: 'Not found',
+    });
   });
-
-   app.use((err, req, res, next) => {
+  app.use((err, req, res, next) => {
     res.status(500).json({
-      status: 500,
       message: 'Something went wrong',
       error: err.message,
     });
   });
 
-    app.listen(PORT, ()=> {
-  console.log(`Server is running on port ${PORT}`);
-    });
-};
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
